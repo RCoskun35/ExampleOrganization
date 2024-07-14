414354c010335d46af057b10da602dd363b106ab
Ridvan <ridvancoskun1688@gmail.com>
2024-07-14 17:01:16 +0300

    AdminLte First Commit

---
+++ ExampleOrganization/ExampleOrganization.Infrastructure/Modules/Module.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOrganization.Infrastructure.Modules
+{
+    public class Module
+    {
+        public int ModuleId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int? ParentId { get; set; }
+    }
+}
